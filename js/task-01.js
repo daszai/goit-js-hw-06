604b8c079b5290776de089c81bda0a81311fa911
Benedykt Powała <benedyktpowala@gmail.com>
2024-08-05 17:44:06 +0200

    aaa

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,10 @@
 "use strict";
 
-console.log();
+var items = document.querySelectorAll("li.item");
+
+console.log("Number of categories : " + items.length);
+
+for (const item of items) {
+  console.log("Category: " + item.children[0].textContent);
+  console.log("Elements: " + item.children[1].children.length);
+}
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+var li;
+
+li = document.createElement("li");
+li.before(document.createElement("li"));
+
+console.log(li);
